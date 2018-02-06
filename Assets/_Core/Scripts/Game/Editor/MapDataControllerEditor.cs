199cde865b28cce6c4dd56fa4563fc5e6dbd8560
NorthernShady <egora@zeptolab.com>
2018-02-07 02:31:24 +0300

    Added base classes for map editor

---
+++ Assets/_Core/Scripts/Game/Editor/MapDataControllerEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapDataController))]
+public class MapDataControllerEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		MapDataController mapDataController = (MapDataController)target;
+
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("Save")) {
+			var asset = Resources.Load<MapData>(mapDataController.mapDataName);
+			EditorUtility.SetDirty(asset);
+			mapDataController.saveMapData();
+			AssetDatabase.SaveAssets();
+		}
+
+		if (GUILayout.Button("Load")) {
+			mapDataController.loadMapData();
+		}
+	}
+}
---
+++ Assets/_Core/Scripts/Game/LevelEditor/MapDataController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDataController : MonoBehaviour {
+
+	[SerializeField]
+	string m_mapDataName;
+
+	public string mapDataName
+	{
+		get {
+			return m_mapDataName;
+		}
+	}
+
+	public void saveMapData()
+	{
+	}
+
+	public void loadMapData()
+	{
+	}
+}
