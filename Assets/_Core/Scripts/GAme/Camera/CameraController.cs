adb43daf2058ac9a5d4177115a55725744193e3f
Georgy Andreev <mirror.life.inside@gmail.com>
2018-02-07 20:38:36 +0300

    Added Camera Follower

---
+++ Assets/_Core/Scripts/GAme/Camera/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField]
+	CameraFollower m_follower = null;
+
+	public float m_cameraShift = 0;
+
+	void Awake()
+	{
+		m_cameraShift = transform.position.y / Mathf.Tan(transform.eulerAngles.x * Mathf.Deg2Rad);
+		transform.position = new Vector3(0.0f, transform.position.y, -m_cameraShift);
+	}
+
+	void OnEnable()
+	{
+		if (m_follower != null)
+			m_follower.OnPositionChanged += onUpdatePosition;
+	}
+
+	void OnDisable()
+	{
+		if (m_follower != null)
+			m_follower.OnPositionChanged -= onUpdatePosition;
+	}
+
+	public void setFollower(CameraFollower follower) {
+		m_follower = follower;
+		follower.OnPositionChanged += onUpdatePosition;
+	}
+
+	void onUpdatePosition(Vector3 position)
+	{
+		transform.position = new Vector3(position.x, transform.position.y, position.z - m_cameraShift);
+	}
+}
---
+++ Assets/_Core/Scripts/GAme/Camera/CameraFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+	public System.Action<Vector3> OnPositionChanged;
+
+	Vector3 m_position;
+
+	void Awake() {
+		m_position = transform.position;
+	}
+
+	void Start() {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (m_position != transform.position) {
+			m_position = transform.position;
+			if (OnPositionChanged != null)
+				OnPositionChanged(m_position);
+		}
+	}
+}
