8f824af857897607260e07fe5ab2124121e29a24
NorthernShady <mirror.life.inside@gmail.com>
2018-02-08 13:08:05 +0300

    Fixed camera system. Added ReturnToPlayer button.

---
+++ Assets/_Core/Scripts/Game/UI/ReturnToPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToPlayer : MonoBehaviour {
+
+	CameraController m_cameraController = null;
+
+	void Awake()
+	{
+		m_cameraController = FindObjectOfType<CameraController>();
+	}
+
+	public void onClick()
+	{
+		m_cameraController.setCameraType(CameraController.CameraType.FOLLOWING);
+	}
+}
