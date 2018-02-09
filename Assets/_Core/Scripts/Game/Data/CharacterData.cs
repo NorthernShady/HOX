19f7e6d0217e3efa5476473f3478b7826f8b17c1
NorthernShady <mirror.life.inside@gmail.com>
2018-02-09 12:05:39 +0300

    Fixed DB

---
+++ Assets/_Core/Scripts/Game/Data/CharacterData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterData {
+
+	public float maxHealth;
+	public float attack;
+	public float defence;
+	public float attackSpeed;
+}
