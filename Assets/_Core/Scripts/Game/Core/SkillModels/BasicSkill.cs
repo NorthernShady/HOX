f8971266684f7faf1e151457525822f98b18ce04
NorthernShady <egora@zeptolab.com>
2018-10-04 19:15:55 +0300

    Working on skills.

--- Assets/_Core/Scripts/Game/Core/SkillModels/BasicSkill.cs
+++ Assets/_Core/Scripts/Game/Core/SkillModels/BasicSkill.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BasicSkill : MonoBehaviour
 {
+    public virtual void activate()
+    {
+    }
 }
 
 [System.Serializable]
---
+++ Assets/_Core/Scripts/Game/UI/ActiveFeatures/ItemFeature.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFeature : ActiveFeature
+{
+	protected Item m_item = null;
+
+	protected override void onFeatureActivated()
+	{
+		m_character.useItem(m_item);
+		m_item = null;
+	}
+}
