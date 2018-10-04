90675f2a4a9354f2dd511bd2aa6bc17bdf36ff1a
NorthernShady <egora@zeptolab.com>
2018-10-04 14:29:28 +0300

    Added well.

---
+++ Assets/_Core/Scripts/Game/Core/SkillModels/BasicSkill.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSkill : MonoBehaviour
+{
+}
+
+[System.Serializable]
+public class Skills
+{
+    public List<BasicSkill> skills = new List<BasicSkill>();
+}
---
+++ Assets/_Core/Scripts/Game/Core/SkillModels/HealAura.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealAura : BasicSkill
+{
+	[SerializeField]
+	float m_healAmount = 1.0f;
+
+	[SerializeField]
+	float m_healTime = 1.0f;
+
+	List<Character> m_characters = new List<Character>();
+
+	private void Start()
+	{
+		StartCoroutine(runAura());	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		var character = getCharacter(other);
+		m_characters.Add(character);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		var character = getCharacter(other);
+		m_characters.Remove(character);
+	}
+
+	private Character getCharacter(Collider other)
+	{
+		var physicalModel = other.GetComponent<BasicPhysicalModel>();
+		var targetObject = physicalModel.targetObject;
+		return targetObject.GetComponent<Character>();
+	}
+
+	IEnumerator runAura()
+	{
+		var delay = new WaitForSeconds(m_healTime);
+
+		while (true)
+		{
+			yield return delay;
+			m_characters.ForEach(x => x.heal(m_healAmount, true));
+		}
+	}
+}
