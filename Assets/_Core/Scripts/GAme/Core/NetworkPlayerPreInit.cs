f3fd97fca4250690eac80cf15612603a60b40545
antonf <antonf@zeptolab.com>
2018-10-05 15:26:02 +0300

    fix

---
+++ Assets/_Core/Scripts/GAme/Core/NetworkPlayerPreInit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayerPreInit : Photon.PunBehaviour, IPunObservable {
+
+    bool m_isInit = false;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        //if (stream)
+    }
+
+}
